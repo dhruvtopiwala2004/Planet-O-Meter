afa1a9d911ffe1454dbe33d993ca10920f698d80
dhruvtopiwala2004 <dhruvtopiwala14@gmail.com>
2026-08-23 19:22:23 -0500

    Phase 1 Complete

--- Journal.md
+++ Journal.md
@@ -1,2 +1,11 @@
 # Journal
 Write your Journal questions and notes here.
+
+#Planet-O-Meter
+##Phase 1
+
+I used the Scanner class to get the user's input.
+I used nextLine() for both the user's name and the planet's name.
+I stored both input in String variables.
+I used those variables in the welcome message.
+I did not have an issue with the Scanner in this phase.
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     static void main() {
-        System.out.println("Hello World!");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+
+        System.out.println("What planet do you want to visit?");
+        String planet = input.nextLine();
+
+        System.out.println("Welcome " + name + "!");
+        System.out.println("You are looking at visiting " + planet + ".");
+
     }
 }
